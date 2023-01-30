3ac2772f5fcdae8b1a4c42950d697aa7f074a785
Paco Ruiz <francisco.javier.ruiz.guijarro@ing.com>
2023-01-30 17:00:06 +0100

    Adding a List component

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,65 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-let count = 0;
+const welcome = {
+  greeting: 'Hello',
+  title: 'React'
+};
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.js',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Refactoring',
+    url: 'https://refactoring.io',
+    author: 'Martin Fowler',
+    num_comments: 5,
+    points: 2,
+    objectID: 1
+  }
+];
 
 function App() {
   console.log("Starting App()");
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h2>Vite + React</h2>
-      <div className="card">
-        <button onClick={() => {
-          console.log("button clicked");
-          count++;
-          console.log("count: " + count);
-        }
-        }>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        This is super cool
-      </p>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id="search" type="text" />
+
+      <List />
     </div>
   )
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+
+    </ul>
+  );
 }
 
 export default App
